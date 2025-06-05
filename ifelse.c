fec121d2a0f33a0e5c23410c1601037e1d9833e1
Jashwanth G <codesjash@gmail.com>
2025-06-05 20:47:19 +0530

    initial commit

---
+++ ifelse.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char ch;
+    ch=getc(stdin);
+    if (ch==A)
+
+    {
+        printf("Your HRA is 45 %");
+
+    }
+    
+    if (ch==B)
+
+    {
+        printf("Your HRA is 40 %");
+    }
+    
+   if (ch==C) 
+
+    {
+       printf("Your HRF is 30 %");
+
+    }
+
+    if (ch==D)
+    {
+        printf("Your HRF is 25 %");
+    }
+
+else:
+printf("Enter the right gradde");
+
+grtch();
+
+}
+    
